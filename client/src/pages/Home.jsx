58fe7e1228ce19039a2a2af46c5f2cf50b8f42fc
Valentin1289 <valibaiacof1289@gmail.com>
2023-10-08 03:48:08 +0300

    start for home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <div className="left-0 right-0 m-auto flex-col gap-4 backdrop-blur-lg rounded-lg bg-gray-700 flex justify-center items-center h-screen">
+            <h2 className="text-md sm-375:text-2xl font-bold text-white">
+              Be the creator of NASA's projects grace to the open science concept
+            </h2>
+            <p className="font-sans text-sm sm-375:text-lg text-white">
+              First you can find more about open scienci in the video bellow then you can start your journey to search for great projects.
+            </p>
+    </div>
   );
 }
